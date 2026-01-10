9210945811dc77e2e53d93170898691f79e48abc
AuraFarmer <vishnunair820@gmail.com>
2026-01-10 17:02:22 +0530

    Updated main.py with my changes

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,24 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from database.database import engine, Base   
+from database import models                  
+
 app = FastAPI()
 
-# List of domains allowed to talk to your API
+Base.metadata.create_all(bind=engine)
+
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    # "*"  <-- Un-comment to allow ALL domains (development only)
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
